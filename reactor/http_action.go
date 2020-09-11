7312c386a986b51b3a2ac74af4607c1f077e7263
Bo Maryniuk <bo@maryniuk.net>
2020-09-11 18:54:01 +0200

    Implement scaffold for the HTTP action

--- reactor/http_action.go
+++ reactor/http_action.go
@@ -1,8 +1,33 @@
 package jenkobs_reactor
 
+import "github.com/streadway/amqp"
+
 /*
 	HTTP Action calls specified URL if criterion meets
 */
 
+// HTTPAction object
 type HTTPAction struct {
+	actionInfo *ActionInfo
+}
+
+// NewHTTPAction constructor
+func NewHTTPAction() *HTTPAction {
+	hta := new(HTTPAction)
+	return hta
+}
+
+// GetAction info on request for this action object
+func (hta *HTTPAction) GetAction() *ActionInfo {
+	return hta.actionInfo
+}
+
+// LoadAction info
+func (hta *HTTPAction) LoadAction(action *ActionInfo) {
+	hta.actionInfo = action
+}
+
+// OnMessage when delivery comes, match the criteria according to the action info
+func (hta *HTTPAction) OnMessage(message *amqp.Delivery) {
+
 }
