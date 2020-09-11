9d721e239180a33bdb3509410d2d7c8946340947
Bo Maryniuk <bo@maryniuk.net>
2020-09-11 18:54:12 +0200

    Implement working scaffold for shell action

--- reactor/shell_action.go
+++ reactor/shell_action.go
@@ -1,8 +1,41 @@
 package jenkobs_reactor
 
+import (
+	"fmt"
+
+	"github.com/streadway/amqp"
+)
+
 /*
 	Shell action calls specific commands, if criterion meets
 */
 
+// ShellAction object
 type ShellAction struct {
+	actionInfo *ActionInfo
+}
+
+// NewShellAction constructor
+func NewShellAction() *ShellAction {
+	shellAction := new(ShellAction)
+	return shellAction
+}
+
+// GetAction info on request for this action object
+func (shact *ShellAction) GetAction() *ActionInfo {
+	return shact.actionInfo
+}
+
+// LoadAction info
+func (shact *ShellAction) LoadAction(action *ActionInfo) {
+	shact.actionInfo = action
+}
+
+// OnMessage when delivery comes, match the criteria according to the action info
+func (shact *ShellAction) OnMessage(message *amqp.Delivery) {
+	if message.RoutingKey == shact.actionInfo.Status {
+		fmt.Println(">>> ", message.RoutingKey)
+		fmt.Println(string(message.Body))
+		fmt.Println("---")
+	}
 }
