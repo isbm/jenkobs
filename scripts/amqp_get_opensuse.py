4360e4c7ce88fa428e95e45dd072637881ca2cdc
Bo Maryniuk <bo@maryniuk.net>
2020-09-11 16:54:44 +0200

    Update example script

--- scripts/amqp_get_opensuse.py
+++ scripts/amqp_get_opensuse.py
@@ -9,17 +9,13 @@ channel.exchange_declare(exchange='pubsub', exchange_type='topic', passive=True,
 
 result = channel.queue_declare("", exclusive=True)
 queue_name = result.method.queue
-
-channel.queue_bind(exchange='pubsub',
-                   queue=queue_name,routing_key='#')
+print("Queue:", queue_name)
+channel.queue_bind(exchange='pubsub', queue=queue_name, routing_key='#')
 
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 def callback(ch, method, properties, body):
     print(" [x] %r:%r" % (method.routing_key, body))
 
-channel.basic_consume(queue_name,
-                      callback,
-                      auto_ack=True)
-
+channel.basic_consume(queue_name, callback, auto_ack=True)
 channel.start_consuming()
