6cc35d8b7030a6a91d1ac21b6b552c0f51eee4b2
Bo Maryniuk <bo@maryniuk.net>
2020-09-17 18:36:25 +0200

    Format URL for request (initial)

--- reactor/http_action.go
+++ reactor/http_action.go
@@ -1,11 +1,11 @@
 package jenkobs_reactor
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"path"
+	"strconv"
 	"strings"
 
 	"github.com/jinzhu/copier"
@@ -17,6 +17,7 @@ import (
 
 // HTTPAction object
 type HTTPAction struct {
+	auth *JenkinsAuth
 	BaseAction
 }
 
@@ -26,16 +27,46 @@ func NewHTTPAction() *HTTPAction {
 	return hta
 }
 
+// Get URL for the action. If the URL is without scheme,
+// then it will be treated as FQDN-less URN, and a default hostname will be used.
+func (hta *HTTPAction) getURL() string {
+	jkFqdn := hta.auth.Fqdn
+	if jkFqdn == "" {
+		jkFqdn = "localhost"
+	}
+	jkPort := hta.auth.Port
+
+	url, ok := hta.actionInfo.Params["url"]
+	if !ok {
+		hta.GetLogger().Errorln("URL is not defined to the HTTP action. Check your configuration.")
+		return ""
+	}
+
+	out := url.(string)
+	if !strings.HasPrefix(out, "http") && strings.Contains(out, "://") {
+		var buff strings.Builder
+		buff.WriteString("https://")
+		buff.WriteString(jkFqdn)
+		if jkPort != 443 && jkPort != 0 {
+			buff.WriteString(":" + strconv.Itoa(jkPort))
+		}
+		buff.WriteString("/" + strings.TrimPrefix(path.Clean(out), "/"))
+		out = buff.String()
+	}
+
+	return out
+}
+
+// SetJenkinsAuth sets the authentication data for Jenkins
+func (hta *HTTPAction) SetJenkinsAuth(auth *JenkinsAuth) {
+	hta.auth = auth
+}
+
 // Perform a HTTP/S request
 func (hta *HTTPAction) request(message *ReactorDelivery) error {
 	query, ok := hta.GetActionInfo().Params["query"].(map[string]interface{})
 	if !ok {
 		return fmt.Errorf("Query is not defined for the HTTP action watching '%s' project", hta.GetActionInfo().Project)
-	}
-
-	url, ok := query["url"].(string)
-	if !ok {
-		return fmt.Errorf("URL was not specified for the HTTP action watching '%s' project", hta.GetActionInfo().Project)
 	}
 
 	method, ok := query["method"].(string)
@@ -46,11 +77,16 @@ func (hta *HTTPAction) request(message *ReactorDelivery) error {
 
 	switch strings.ToLower(method) {
 	case "post":
-		req, _ := json.Marshal(map[string]string{})
-		resp, err := http.Post(fmt.Sprintf("http://localhost:8080%s", url), "application/json", bytes.NewBuffer(req))
+		request, err := http.NewRequest("POST", hta.getURL(), nil)
 		if err != nil {
 			return err
 		}
+		request.SetBasicAuth(hta.auth.User, hta.auth.Token)
+		resp, err := http.DefaultClient.Do(request)
+		if err != nil {
+			return err
+		}
+
 		defer resp.Body.Close()
 		body, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
